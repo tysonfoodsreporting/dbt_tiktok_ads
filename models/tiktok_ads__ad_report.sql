14f208274384ab5187874bdfab4ed6c31779ee6b
PranavB-MBM <pranavb@mediabymother.com>
2026-07-02 11:30:14 +0530

    Modified video metrics in tiktok_ads__ad_report.sql
    Updated video metrics calculations in the ad report SQL.

--- models/tiktok_ads__ad_report.sql
+++ models/tiktok_ads__ad_report.sql
@@ -72,10 +72,12 @@ aggregated as (
         sum(hourly.profile_visits) as profile_visits,
         sum(hourly.follows) as follows,
     
-        --sum(hourly.engagements) as engagements,
+        -- Standardized video metrics
         sum(hourly.video_play_actions) as video_plays,
-        sum(hourly.video_play_actions) as video_views,
-    
+        sum(hourly.video_watched_2_s) as video_views,
+
+        --sum(hourly.engagements) as engagements,
+
         sum(hourly.video_watched_2_s) as video_watched_2_s,
         sum(hourly.video_watched_6_s) as video_watched_6_s,
         sum(hourly.video_views_p_25) as video_views_p_25,
